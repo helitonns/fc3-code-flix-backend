7b3c6852f6e1b3552d8083dbd6ed5637d81c2926
helitonns <helitonns@gmail.com>
2024-06-24 14:21:18 -0400

    TDD da entidade video

---
+++ domain/src/test/java/com/fullcycle/admin/catalogo/domain/video/VideoTeste.java
@@ -0,0 +1,68 @@
+package com.fullcycle.admin.catalogo.domain.video;
+
+import java.time.Year;
+import java.util.Set;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import com.fullcycle.admin.catalogo.domain.entity.castmember.CastMemberID;
+import com.fullcycle.admin.catalogo.domain.entity.category.CategoryID;
+import com.fullcycle.admin.catalogo.domain.entity.genre.GenreID;
+import com.fullcycle.admin.catalogo.domain.validation.handler.ThrowsValidationHandler;
+
+public class VideoTeste {
+
+    @Test
+    public void givenValidParams_whenCallsNewVideo_shouldInstantiate(){
+        //given
+        final var expectedTitle = "System Design Interview";
+        final var expectedDescription = """
+            O estudo apresentadeo apresentado tem fins educaionais e representa nossas opiniões pessoais.
+            Esse vídeo faz parte da Imessão Full Stack && Full Cycle. Para acessar todas as aulas, lives
+            e dessafios, acesse: https://imerssao.fullcycle.com.br
+            """;
+        final var expectedLaunchedAt = Year.of(2022);
+        final var expectedDuration = 120.10;
+        final var expectedOpened = false;
+        final var expectedPublished = false;
+        final var expectedRating = Rating.L;
+        final var expectedCategories = Set.of(CategoryID.unique());
+        final var expectedGenres = Set.of(GenreID.unique());
+        final var expectedMembers = Set.of(CastMemberID.unique());
+
+        //when
+        final var actualVideo = Video.newVideo(
+            expectedTitle,
+            expectedDescription,
+            expectedLaunchedAt,
+            expectedDuration,
+            expectedOpened,
+            expectedPublished,
+            expectedRating,
+            expectedCategories,
+            expectedGenres,
+            expectedMembers
+        );
+
+        //then
+        Assertions.assertNotNull(actualVideo);
+        Assertions.assertNotNull(actualVideo.getId());
+        Assertions.assertEquals(expectedTitle, actualVideo.getTitle());
+        Assertions.assertEquals(expectedDescription, actualVideo.getDescription());
+        Assertions.assertEquals(expectedLaunchedAt, actualVideo.getLaunchedAt());
+        Assertions.assertEquals(expectedDuration, actualVideo.getDuration());
+        Assertions.assertEquals(expectedOpened, actualVideo.getOpened());
+        Assertions.assertEquals(expectedPublished, actualVideo.getPublished());
+        Assertions.assertEquals(expectedRating, actualVideo.getRating());
+        Assertions.assertEquals(expectedCategories, actualVideo.getCategories());
+        Assertions.assertEquals(expectedGenres, actualVideo.getGenres());
+        Assertions.assertEquals(expectedMembers, actualVideo.getCastMembers());
+        Assertions.assertTrue(actualVideo.getVideo().isEmpty());
+        Assertions.assertTrue(actualVideo.getTrailer().isEmpty());
+        Assertions.assertTrue(actualVideo.getBanner().isEmpty());
+        Assertions.assertTrue(actualVideo.getThumbnail().isEmpty());
+        Assertions.assertTrue(actualVideo.getThumbnailHalf().isEmpty());
+
+        Assertions.assertDoesNotThrow(()-> actualVideo.validate(new ThrowsValidationHandler()));
+
+    }
+}
