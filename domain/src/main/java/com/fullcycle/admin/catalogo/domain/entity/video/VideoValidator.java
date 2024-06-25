4bc0f85960c0fc27e42cee0bf4f791dfdef2be5b
helitonns <helitonns@gmail.com>
2024-06-25 16:44:33 -0400

    Implementando o video validator

---
+++ domain/src/main/java/com/fullcycle/admin/catalogo/domain/entity/video/VideoValidator.java
@@ -0,0 +1,80 @@
+package com.fullcycle.admin.catalogo.domain.entity.video;
+
+import java.util.Objects;
+import com.fullcycle.admin.catalogo.domain.validation.Error;
+import com.fullcycle.admin.catalogo.domain.validation.ValidationHandler;
+import com.fullcycle.admin.catalogo.domain.validation.Validator;
+
+public class VideoValidator extends Validator {
+
+    private Video video;
+    private static final int TITLE_MAX_LENGTH = 255;
+    private static final int DESCRIPTION_MAX_LENGTH = 4_000;
+
+
+    public VideoValidator(final Video video, final ValidationHandler handler) {
+        super(handler);
+
+        this.video = video;
+    }
+
+    @Override
+    public void validate() {
+        checkTitleConstraints();
+        checkDescriptionConstraints();
+        checkLaunchedAtConstraints();
+        checkRatingConstraints();
+    }
+
+    private void checkTitleConstraints() {
+        final var title = this.video.getTitle();
+        
+        if (title == null) {
+            this.validationHandler().append(new Error("'title' should not be null"));
+            return;
+        }
+
+        if (title.isBlank()) {
+            this.validationHandler().append(new Error("'title' should not be empty"));
+            return;
+        }
+
+        final int length = title.trim().length();
+        if (length > TITLE_MAX_LENGTH) {
+            this.validationHandler().append(new Error("'title' must be between 1 and 255 characters"));
+        }
+    }
+    
+    private void checkDescriptionConstraints() {
+        final var description = this.video.getDescription();
+        
+        if (description == null) {
+            this.validationHandler().append(new Error("'description' should not be null"));
+            return;
+        }
+
+        if (description.isBlank()) {
+            this.validationHandler().append(new Error("'description' should not be empty"));
+            return;
+        }
+
+        final int length = description.trim().length();
+        if (length > DESCRIPTION_MAX_LENGTH) {
+            this.validationHandler().append(new Error("'description' must be between 1 and 4000 characters"));
+        }
+    }
+    
+    private void checkLaunchedAtConstraints() {
+        if (Objects.isNull(this.video.getLaunchedAt())) {
+            this.validationHandler().append(new Error("'launchedAt' should not be null"));
+        }
+    }
+    
+    private void checkRatingConstraints() {
+        if (Objects.isNull(this.video.getRating())) {
+            this.validationHandler().append(new Error("'rating' should not be null"));
+        }
+    }
+
+
+}
--- domain/src/test/java/com/fullcycle/admin/catalogo/domain/video/VideoValidatorTest.java
+++ domain/src/test/java/com/fullcycle/admin/catalogo/domain/video/VideoValidatorTest.java
@@ -11,6 +11,7 @@ import com.fullcycle.admin.catalogo.domain.entity.video.Rating;
 import com.fullcycle.admin.catalogo.domain.entity.video.Video;
 import com.fullcycle.admin.catalogo.domain.exceptions.DomainException;
 import com.fullcycle.admin.catalogo.domain.validation.handler.ThrowsValidationHandler;
+import com.fullcycle.admin.catalogo.domain.entity.video.VideoValidator;
 
 public class VideoValidatorTest {
 
@@ -104,17 +105,20 @@ public class VideoValidatorTest {
 
     @Test
     public void givenTitleWithLengthGreater255_whenCallsValidator_shouldReceiveError(){
-        //given
+        // given
         final var expectedTitle = """
-            O estudo apresentadeo apresentado tem fins educaionais e representa nossas opiniões pessoais.
-            Esse vídeo faz parte da Imessão Full Stack && Full Cycle. Para acessar todas as aulas, lives
-            e dessafios, acesse: https://imerssao.fullcycle.com.br
-            """;;
+                Disclaimer: o estudo de caso apresentado tem fins educacionais e representa nossas opiniões pessoais.
+                Esse vídeo faz parte da Imersão Full Stack && Full Cycle.
+                Esse vídeo faz parte da Imersão Full Stack && Full Cycle.
+                Para acessar todas as aulas, lives e desafios, acesse:
+                https://imersao.fullcycle.com.br/
+                """;
         final var expectedDescription = """
-            O estudo apresentadeo apresentado tem fins educaionais e representa nossas opiniões pessoais.
-            Esse vídeo faz parte da Imessão Full Stack && Full Cycle. Para acessar todas as aulas, lives
-            e dessafios, acesse: https://imerssao.fullcycle.com.br.
-            """;
+                Disclaimer: o estudo de caso apresentado tem fins educacionais e representa nossas opiniões pessoais.
+                Esse vídeo faz parte da Imersão Full Stack && Full Cycle.
+                Para acessar todas as aulas, lives e desafios, acesse:
+                https://imersao.fullcycle.com.br/
+                """;
         final var expectedLaunchedAt = Year.of(2022);
         final var expectedDuration = 120.10;
         final var expectedOpened = false;
@@ -128,24 +132,27 @@ public class VideoValidatorTest {
         final var expectedErrorMessage = "'title' must be between 1 and 255 characters";
 
         final var actualVideo = Video.newVideo(
-            expectedTitle,
-            expectedDescription,
-            expectedLaunchedAt,
-            expectedDuration,
-            expectedRating,
-            expectedOpened,
-            expectedPublished,
-            expectedCategories,
-            expectedGenres,
-            expectedMembers
+                expectedTitle,
+                expectedDescription,
+                expectedLaunchedAt,
+                expectedDuration,
+                expectedRating,
+                expectedOpened,
+                expectedPublished,
+                expectedCategories,
+                expectedGenres,
+                expectedMembers
         );
-        
-        final var validator = new VideoValidator(actualVideo, new ThrowsValidationHandler());
-        
-        //when
-        final var actualError = Assertions.assertThrows(DomainException.class, ()-> validator.validate());
 
-        //then
+        final var validator = new VideoValidator(actualVideo, new ThrowsValidationHandler());
+
+        // when
+        final var actualError = Assertions.assertThrows(
+                DomainException.class,
+                () -> validator.validate()
+        );
+
+        // then
         Assertions.assertEquals(expectedErrorCount, actualError.getErrors().size());
         Assertions.assertEquals(expectedErrorMessage, actualError.getErrors().get(0).message());
     }
@@ -165,7 +172,7 @@ public class VideoValidatorTest {
         final var expectedMembers = Set.of(CastMemberID.unique());
 
         final var expectedErrorCount = 1;
-        final var expectedErrorMessage = "'description' sholud not be null";
+        final var expectedErrorMessage = "'description' should not be null";
 
         final var actualVideo = Video.newVideo(
             expectedTitle,
@@ -205,7 +212,7 @@ public class VideoValidatorTest {
         final var expectedMembers = Set.of(CastMemberID.unique());
 
         final var expectedErrorCount = 1;
-        final var expectedErrorMessage = "'description' sholud not be empty";
+        final var expectedErrorMessage = "'description' should not be empty";
 
         final var actualVideo = Video.newVideo(
             expectedTitle,
@@ -232,56 +239,28 @@ public class VideoValidatorTest {
 
     @Test
     public void givenDescriptionWithLengthGreater4000_whenCallsValidator_shouldReceiveError(){
-        //given
-        final var expectedTitle = "System Design Interview";
-        final String expectedDescription = """
-            O estudo apresentadeo apresentado tem fins educaionais e representa nossas opiniões pessoais.
-            Esse vídeo faz parte da Imessão Full Stack && Full Cycle. Para acessar todas as aulas, lives
-            e dessafios, acesse: https://imerssao.fullcycle.com.br
-            O estudo apresentadeo apresentado tem fins educaionais e representa nossas opiniões pessoais.
-            Esse vídeo faz parte da Imessão Full Stack && Full Cycle. Para acessar todas as aulas, lives
-            e dessafios, acesse: https://imerssao.fullcycle.com.br
-            O estudo apresentadeo apresentado tem fins educaionais e representa nossas opiniões pessoais.
-            Esse vídeo faz parte da Imessão Full Stack && Full Cycle. Para acessar todas as aulas, lives
-            e dessafios, acesse: https://imerssao.fullcycle.com.br
-            O estudo apresentadeo apresentado tem fins educaionais e representa nossas opiniões pessoais.
-            Esse vídeo faz parte da Imessão Full Stack && Full Cycle. Para acessar todas as aulas, lives
-            e dessafios, acesse: https://imerssao.fullcycle.com.br
-            O estudo apresentadeo apresentado tem fins educaionais e representa nossas opiniões pessoais.
-            Esse vídeo faz parte da Imessão Full Stack && Full Cycle. Para acessar todas as aulas, lives
-            e dessafios, acesse: https://imerssao.fullcycle.com.br
-            O estudo apresentadeo apresentado tem fins educaionais e representa nossas opiniões pessoais.
-            Esse vídeo faz parte da Imessão Full Stack && Full Cycle. Para acessar todas as aulas, lives
-            e dessafios, acesse: https://imerssao.fullcycle.com.br
-            O estudo apresentadeo apresentado tem fins educaionais e representa nossas opiniões pessoais.
-            Esse vídeo faz parte da Imessão Full Stack && Full Cycle. Para acessar todas as aulas, lives
-            e dessafios, acesse: https://imerssao.fullcycle.com.br
-            O estudo apresentadeo apresentado tem fins educaionais e representa nossas opiniões pessoais.
-            Esse vídeo faz parte da Imessão Full Stack && Full Cycle. Para acessar todas as aulas, lives
-            e dessafios, acesse: https://imerssao.fullcycle.com.br
-            O estudo apresentadeo apresentado tem fins educaionais e representa nossas opiniões pessoais.
-            Esse vídeo faz parte da Imessão Full Stack && Full Cycle. Para acessar todas as aulas, lives
-            e dessafios, acesse: https://imerssao.fullcycle.com.br
-            O estudo apresentadeo apresentado tem fins educaionais e representa nossas opiniões pessoais.
-            Esse vídeo faz parte da Imessão Full Stack && Full Cycle. Para acessar todas as aulas, lives
-            e dessafios, acesse: https://imerssao.fullcycle.com.br
-            O estudo apresentadeo apresentado tem fins educaionais e representa nossas opiniões pessoais.
-            Esse vídeo faz parte da Imessão Full Stack && Full Cycle. Para acessar todas as aulas, lives
-            e dessafios, acesse: https://imerssao.fullcycle.com.br
-            O estudo apresentadeo apresentado tem fins educaionais e representa nossas opiniões pessoais.
-            Esse vídeo faz parte da Imessão Full Stack && Full Cycle. Para acessar todas as aulas, lives
-            e dessafios, acesse: https://imerssao.fullcycle.com.br
-            O estudo apresentadeo apresentado tem fins educaionais e representa nossas opiniões pessoais.
-            Esse vídeo faz parte da Imessão Full Stack && Full Cycle. Para acessar todas as aulas, lives
-            e dessafios, acesse: https://imerssao.fullcycle.com.br
-            O estudo apresentadeo apresentado tem fins educaionais e representa nossas opiniões pessoais.
-            Esse vídeo faz parte da Imessão Full Stack && Full Cycle. Para acessar todas as aulas, lives
-            e dessafios, acesse: https://imerssao.fullcycle.com.br
-            O estudo apresentadeo apresentado tem fins educaionais e representa nossas opiniões pessoais.
-            Esse vídeo faz parte da Imessão Full Stack && Full Cycle. Para acessar todas as aulas, lives
-            e dessafios, acesse: https://imerssao.fullcycle.com.br
-            """;
-        final Year expectedLaunchedAt = Year.of(2022);
+        // given
+        final var expectedTitle = "System Design Interviews";
+        final var expectedDescription = """
+        A prática cotidiana prova que o surgimento do comércio virtual não pode mais se dissociar das posturas dos órgãos dirigentes com relação às suas atribuições. As experiências acumuladas demonstram que o aumento do diálogo entre os diferentes setores produtivos cumpre um papel essencial na formulação das diretrizes de desenvolvimento para o futuro. Assim mesmo, o acompanhamento das preferências de consumo obstaculiza a apreciação da importância do investimento em reciclagem técnica. Por conseguinte, o julgamento imparcial das eventualidades auxilia a preparação e a composição dos paradigmas corporativos. Do mesmo modo, a consulta aos diversos militantes garante a contribuição de um grupo importante na determinação da gestão inovadora da qual fazemos parte.
+                            
+        Caros amigos, o desenvolvimento contínuo de distintas formas de atuação prepara-nos para enfrentar situações atípicas decorrentes das direções preferenciais no sentido do progresso. Não obstante, a contínua expansão de nossa atividade facilita a criação do retorno esperado a longo prazo. No mundo atual, a mobilidade dos capitais internacionais maximiza as possibilidades por conta dos relacionamentos verticais entre as hierarquias.
+                
+        Por outro lado, a competitividade nas transações comerciais deve passar por modificações independentemente dos índices pretendidos. A nível organizacional, a estrutura atual da organização talvez venha a ressaltar a relatividade do sistema de participação geral. Evidentemente, a percepção das dificuldades estende o alcance e a importância dos métodos utilizados na avaliação de resultados. No entanto, não podemos esquecer que a consolidação das estruturas pode nos levar a considerar a reestruturação das regras de conduta normativas.
+                
+        Ainda assim, existem dúvidas a respeito de como o fenômeno da Internet possibilita uma melhor visão global dos conhecimentos estratégicos para atingir a excelência. O cuidado em identificar pontos críticos na hegemonia do ambiente político ainda não demonstrou convincentemente que vai participar na mudança das condições inegavelmente apropriadas. Pensando mais a longo prazo, o comprometimento entre as equipes estimula a padronização de todos os recursos funcionais envolvidos. Todas estas questões, devidamente ponderadas, levantam dúvidas sobre se a constante divulgação das informações aponta para a melhoria do levantamento das variáveis envolvidas. Gostaria de enfatizar que a crescente influência da mídia causa impacto indireto na reavaliação dos modos de operação convencionais.
+                
+        O incentivo ao avanço tecnológico, assim como a necessidade de renovação processual agrega valor ao estabelecimento das formas de ação. Podemos já vislumbrar o modo pelo qual o novo modelo estrutural aqui preconizado é uma das consequências de alternativas às soluções ortodoxas. Nunca é demais lembrar o peso e o significado destes problemas, uma vez que a execução dos pontos do programa promove a alavancagem dos procedimentos normalmente adotados. Desta maneira, a expansão dos mercados mundiais nos obriga à análise do fluxo de informações.
+                
+        É importante questionar o quanto o início da atividade geral de formação de atitudes acarreta um processo de reformulação e modernização dos níveis de motivação departamental. Neste sentido, a complexidade dos estudos efetuados oferece uma interessante oportunidade para verificação das condições financeiras e administrativas exigidas. O empenho em analisar a determinação clara de objetivos exige a precisão e a definição das diversas correntes de pensamento. O que temos que ter sempre em mente é que a adoção de políticas descentralizadoras faz parte de um processo de gerenciamento do impacto na agilidade decisória. É claro que a valorização de fatores subjetivos desafia a capacidade de equalização das novas proposições.
+                
+        Percebemos, cada vez mais, que a revolução dos costumes apresenta tendências no sentido de aprovar a manutenção do processo de comunicação como um todo. Acima de tudo, é fundamental ressaltar que o entendimento das metas propostas afeta positivamente a correta previsão do sistema de formação de quadros que corresponde às necessidades. Todavia, o consenso sobre a necessidade de qualificação assume importantes posições no estabelecimento do orçamento setorial. A certificação de metodologias que nos auxiliam a lidar com o desafiador cenário globalizado representa uma abertura para a melhoria do remanejamento dos quadros funcionais.
+                
+        A prática cotidiana prova que o surgimento do comércio virtual deve passar por modificações independentemente das condições inegavelmente apropriadas. Neste sentido, a percepção das dificuldades cumpre um papel essencial na formulação das diretrizes de desenvolvimento para o futuro. No mundo atual, o acompanhamento das preferências de consumo garante a contribuição de um grupo importante na determinação das direções preferenciais no sentido do progresso. Por conseguinte, o julgamento imparcial das eventualidades auxilia a preparação e a composição da gestão inovadora da qual fazemos parte. Ainda assim, existem dúvidas a respeito de como a necessidade de renovação processual promove a alavancagem das novas proposições.
+                
+        Pensando mais a longo prazo, a determinação clara de objetivos ainda não demonstrou convincentemente que vai participar na mudança dos níveis de motivação departamental. Não obstante, o comprometimento entre as equipes é uma das consequências das formas de ação. Assim mesmo, o desafiador cenário globalizado maximiza as possibilidades por conta dos relacionamentos verticais entre as hierarquias.
+        """;
+        final var expectedLaunchedAt = Year.of(2022);
         final var expectedDuration = 120.10;
         final var expectedOpened = false;
         final var expectedPublished = false;
@@ -291,27 +270,30 @@ public class VideoValidatorTest {
         final var expectedMembers = Set.of(CastMemberID.unique());
 
         final var expectedErrorCount = 1;
-        final var expectedErrorMessage = "'description' must be between 1 and 4000 characters";;
+        final var expectedErrorMessage = "'description' must be between 1 and 4000 characters";
 
         final var actualVideo = Video.newVideo(
-            expectedTitle,
-            expectedDescription,
-            expectedLaunchedAt,
-            expectedDuration,
-            expectedRating,
-            expectedOpened,
-            expectedPublished,
-            expectedCategories,
-            expectedGenres,
-            expectedMembers
+                expectedTitle,
+                expectedDescription,
+                expectedLaunchedAt,
+                expectedDuration,
+                expectedRating,
+                expectedOpened,
+                expectedPublished,
+                expectedCategories,
+                expectedGenres,
+                expectedMembers
         );
-        
-        final var validator = new VideoValidator(actualVideo, new ThrowsValidationHandler());
-        
-        //when
-        final var actualError = Assertions.assertThrows(DomainException.class, ()-> validator.validate());
 
-        //then
+        final var validator = new VideoValidator(actualVideo, new ThrowsValidationHandler());
+
+        // when
+        final var actualError = Assertions.assertThrows(
+                DomainException.class,
+                () -> validator.validate()
+        );
+
+        // then
         Assertions.assertEquals(expectedErrorCount, actualError.getErrors().size());
         Assertions.assertEquals(expectedErrorMessage, actualError.getErrors().get(0).message());
     }
@@ -335,7 +317,7 @@ public class VideoValidatorTest {
         final var expectedMembers = Set.of(CastMemberID.unique());
 
         final var expectedErrorCount = 1;
-        final var expectedErrorMessage = "'launchedAt' sholud not be null";
+        final var expectedErrorMessage = "'launchedAt' should not be null";
 
         final var actualVideo = Video.newVideo(
             expectedTitle,
@@ -379,7 +361,7 @@ public class VideoValidatorTest {
         final var expectedMembers = Set.of(CastMemberID.unique());
 
         final var expectedErrorCount = 1;
-        final var expectedErrorMessage = "'rating' sholud not be null";
+        final var expectedErrorMessage = "'rating' should not be null";
 
         final var actualVideo = Video.newVideo(
             expectedTitle,
