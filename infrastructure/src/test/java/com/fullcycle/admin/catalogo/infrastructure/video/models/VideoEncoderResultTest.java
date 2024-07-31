f57de5161d603049e8e3a1246e46922e390a1043
helitonns <helitonns@gmail.com>
2024-07-31 16:47:13 -0400

    Testes de serializacao das models do listener de encoder result.

--- infrastructure/src/test/java/com/fullcycle/admin/catalogo/infrastructure/video/models/VideoEncoderResultTest.java
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/infrastructure/video/models/VideoEncoderResultTest.java
@@ -12,120 +12,114 @@ import java.io.IOException;
 @JacksonTest
 public class VideoEncoderResultTest {
 
-    @Autowired
-    private JacksonTester<VideoEncoderResult> json;
+        @Autowired
+        private JacksonTester<VideoEncoderResult> json;
 
-    @Test
-    public void testUnmarshallSuccessResult() throws IOException {
-        // given
-        final var expectedId = IdUtils.uuid();
-        final var expectedOutputBucket = "codeeducationtest";
-        final var expectedStatus = "COMPLETED";
-        final var expectedEncoderVideoFolder = "anyfolder";
-        final var expectedResourceId = IdUtils.uuid();
-        final var expectedFilePath = "any.mp4";
-        final var expectedMetadata =
-                new VideoMetadata(expectedEncoderVideoFolder, expectedResourceId, expectedFilePath);
+        @Test
+        public void testUnmarshallSuccessResult() throws IOException {
+                // given
+                final var expectedId = IdUtils.uuid();
+                final var expectedOutputBucket = "codeeducationtest";
+                final var expectedStatus = "COMPLETED";
+                final var expectedEncoderVideoFolder = "anyfolder";
+                final var expectedResourceId = IdUtils.uuid();
+                final var expectedFilePath = "any.mp4";
+                final var expectedMetadata = new VideoMetadata(expectedEncoderVideoFolder,expectedResourceId,expectedFilePath);
 
-        final var json = """
-                    {
-                      "status": "%s",
-                      "id": "%s",
-                      "output_bucket_path": "%s",
-                      "video": {
-                        "encoded_video_folder": "%s",
-                        "resource_id": "%s",
-                        "file_path": "%s"
-                      }
-                    }
-                """.formatted(expectedStatus, expectedId, expectedOutputBucket,
-                expectedEncoderVideoFolder, expectedResourceId, expectedFilePath);
+                final var json = """
+                {
+                        "status": "%s",
+                        "id": "%s",
+                        "output_bucket_path": "%s",
+                        "video": {
+                                "encoded_video_folder": "%s",
+                                "resource_id": "%s",
+                                "file_path": "%s"
+                        }
+                }
+                """.formatted(expectedStatus,expectedId,expectedOutputBucket,expectedEncoderVideoFolder, expectedResourceId, expectedFilePath);
 
-        // when
-        final var actualResult = this.json.parse(json);
+                // when
+                final var actualResult = this.json.parse(json);
 
-        Assertions.assertThat(actualResult)
-                .isInstanceOf(VideoEncoderCompleted.class)
-                .hasFieldOrPropertyWithValue("id", expectedId)
-                .hasFieldOrPropertyWithValue("outputBucket", expectedOutputBucket)
-                .hasFieldOrPropertyWithValue("video", expectedMetadata);
-    }
+                Assertions.assertThat(actualResult).isInstanceOf(VideoEncoderCompleted.class)
+                        .hasFieldOrPropertyWithValue("id", expectedId)
+                        .hasFieldOrPropertyWithValue("outputBucket", expectedOutputBucket)
+                        .hasFieldOrPropertyWithValue("video", expectedMetadata);
+        }
 
-    @Test
-    public void testMarshallSuccessResult() throws IOException {
-        // given
-        final var expectedId = IdUtils.uuid();
-        final var expectedOutputBucket = "codeeducationtest";
-        final var expectedStatus = "COMPLETED";
-        final var expectedEncoderVideoFolder = "anyfolder";
-        final var expectedResourceId = IdUtils.uuid();
-        final var expectedFilePath = "any.mp4";
-        final var expectedMetadata =
-                new VideoMetadata(expectedEncoderVideoFolder, expectedResourceId, expectedFilePath);
+        @Test
+        public void testMarshallSuccessResult() throws IOException {
+                // given
+                final var expectedId = IdUtils.uuid();
+                final var expectedOutputBucket = "codeeducationtest";
+                final var expectedStatus = "COMPLETED";
+                final var expectedEncoderVideoFolder = "anyfolder";
+                final var expectedResourceId = IdUtils.uuid();
+                final var expectedFilePath = "any.mp4";
+                final var expectedMetadata = new VideoMetadata(expectedEncoderVideoFolder,expectedResourceId, expectedFilePath);
 
-        final var aResult = new VideoEncoderCompleted(expectedId, expectedOutputBucket, expectedMetadata);
+                final var aResult = new VideoEncoderCompleted(expectedId, expectedOutputBucket,expectedMetadata);
 
-        // when
-        final var actualResult = this.json.write(aResult);
+                // when
+                final var actualResult = this.json.write(aResult);
 
-        Assertions.assertThat(actualResult)
-                .hasJsonPathValue("$.id", expectedId)
-                .hasJsonPathValue("$.output_bucket_path", expectedOutputBucket)
-                .hasJsonPathValue("$.status", expectedStatus)
-                .hasJsonPathValue("$.video.encoded_video_folder", expectedEncoderVideoFolder)
-                .hasJsonPathValue("$.video.resource_id", expectedResourceId)
-                .hasJsonPathValue("$.video.file_path", expectedFilePath);
-    }
+                Assertions.assertThat(actualResult)
+                        .hasJsonPathValue("$.id", expectedId)
+                        .hasJsonPathValue("$.output_bucket_path", expectedOutputBucket)
+                        .hasJsonPathValue("$.status", expectedStatus)
+                        .hasJsonPathValue("$.video.encoded_video_folder", expectedEncoderVideoFolder)
+                        .hasJsonPathValue("$.video.resource_id", expectedResourceId)
+                        .hasJsonPathValue("$.video.file_path", expectedFilePath);
+        }
 
-    @Test
-    public void testUnmarshallErrorResult() throws IOException {
-        // given
-        final var expectedMessage = "Resource not found";
-        final var expectedStatus = "ERROR";
-        final var expectedResourceId = IdUtils.uuid();
-        final var expectedFilePath = "any.mp4";
-        final var expectedVideoMessage =
-                new VideoMessage(expectedResourceId, expectedFilePath);
+        @Test
+        public void testUnmarshallErrorResult() throws IOException {
+                // given
+                final var expectedMessage = "Resource not found";
+                final var expectedStatus = "ERROR";
+                final var expectedResourceId = IdUtils.uuid();
+                final var expectedFilePath = "any.mp4";
+                final var expectedVideoMessage = new VideoMessage(expectedResourceId, expectedFilePath);
 
-        final var json = """
-                    {
-                      "status": "%s",
-                      "error": "%s",
-                      "message": {
-                        "resource_id": "%s",
-                        "file_path": "%s"
-                      }
-                    }
-                """.formatted(expectedStatus, expectedMessage, expectedResourceId, expectedFilePath);
+                final var json = """
+                {
+                        "status": "%s",
+                        "error": "%s",
+                        "message": {
+                                "resource_id": "%s",
+                                "file_path": "%s"
+                        }
+                }
+                """.formatted(expectedStatus, expectedMessage, expectedResourceId,expectedFilePath);
 
-        // when
-        final var actualResult = this.json.parse(json);
+                // when
+                final var actualResult = this.json.parse(json);
 
-        Assertions.assertThat(actualResult)
-                .isInstanceOf(VideoEncoderError.class)
-                .hasFieldOrPropertyWithValue("error", expectedMessage)
-                .hasFieldOrPropertyWithValue("message", expectedVideoMessage);
-    }
+                Assertions.assertThat(actualResult)
+                        .isInstanceOf(VideoEncoderError.class)
+                        .hasFieldOrPropertyWithValue("error", expectedMessage)
+                        .hasFieldOrPropertyWithValue("message", expectedVideoMessage);
+        }
 
-    @Test
-    public void testMarshallErrorResult() throws IOException {
-        // given
-        final var expectedMessage = "Resource not found";
-        final var expectedStatus = "ERROR";
-        final var expectedResourceId = IdUtils.uuid();
-        final var expectedFilePath = "any.mp4";
-        final var expectedVideoMessage =
-                new VideoMessage(expectedResourceId, expectedFilePath);
+        @Test
+        public void testMarshallErrorResult() throws IOException {
+                // given
+                final var expectedMessage = "Resource not found";
+                final var expectedStatus = "ERROR";
+                final var expectedResourceId = IdUtils.uuid();
+                final var expectedFilePath = "any.mp4";
+                final var expectedVideoMessage = new VideoMessage(expectedResourceId, expectedFilePath);
 
-        final var aResult = new VideoEncoderError(expectedVideoMessage, expectedMessage);
+                final var aResult = new VideoEncoderError(expectedVideoMessage, expectedMessage);
 
-        // when
-        final var actualResult = this.json.write(aResult);
+                // when
+                final var actualResult = this.json.write(aResult);
 
-        Assertions.assertThat(actualResult)
-                .hasJsonPathValue("$.status", expectedStatus)
-                .hasJsonPathValue("$.error", expectedMessage)
-                .hasJsonPathValue("$.message.resource_id", expectedResourceId)
-                .hasJsonPathValue("$.message.file_path", expectedFilePath);
-    }
+                Assertions.assertThat(actualResult)
+                        .hasJsonPathValue("$.status", expectedStatus)
+                        .hasJsonPathValue("$.error", expectedMessage)
+                        .hasJsonPathValue("$.message.resource_id", expectedResourceId)
+                        .hasJsonPathValue("$.message.file_path", expectedFilePath);
+        }
 }
